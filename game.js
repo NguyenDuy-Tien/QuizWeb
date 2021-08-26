6694ac85bff16ab54cc09f44019cd89cb51cf744
tien.nd194857@sis.hust.edu.vn <tien.nd194857@sis.hust.edu.vn>
2021-08-26 21:30:36 +0700

    Load next question & check validation

---
+++ game.js
@@ -0,0 +1,123 @@
+const question = document.getElementsByClassName("question")[0];
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+let c = 0;
+let questions = [];
+let activate = false;
+// fetch("questions.json")
+//     .then(res => res.json())
+//     .then(loadedQuestions => {
+//         questions = loadedQuestions;
+//         startGame();
+//     })
+//     .catch(err => console.error(err))
+
+fetch("https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple")
+    .then(res => res.json())
+    .then(loadedQuestions => {
+        console.log(loadedQuestions.results);
+        questions = loadedQuestions.results.map(loadedQuestion => {
+            const formattedQuestion = {
+                question: loadedQuestion.question
+            };
+
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            const indexCorrect = Math.floor(Math.random() * 3) + 1;
+            formattedQuestion.answer = indexCorrect;
+            answerChoices.splice(indexCorrect - 1, 0, loadedQuestion.correct_answer);
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion["choice" + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        })
+        startGame();
+    })
+    .catch(err => console.error(err))
+
+
+const CORRECT_POINT = 10;
+const NUM_QUESTIONS = 4;
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    if (availableQuestions.length > 0) {
+        getNewQuestion();
+        game.classList.remove('hidden');
+        loader.classList.add('hidden');
+    }
+
+}
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= NUM_QUESTIONS) {
+        console.log(availableQuestions.length);
+        console.log(questionCounter);
+        console.log(NUM_QUESTIONS);
+        localStorage.setItem('lastScore', score);
+        window.location.href = "/end.html";
+        return;
+    }
+
+    questionCounter++;
+    if (questionCounter == NUM_QUESTIONS) {
+        document.getElementById('next-btn').innerHTML = "Finish";
+    }
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    })
+
+    availableQuestions.splice(questionIndex, 1);
+
+    acceptingAnswers = true;
+    document.getElementById('next-btn').disabled = true;
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+        console.log("Answer: " + currentQuestion.answer);
+        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+        selectedChoice.parentElement.classList.add(classToApply);
+        choices[currentQuestion.answer - 1].parentElement.classList.add('correct');
+        document.getElementById('next-btn').disabled = false;
+        if (selectedAnswer == currentQuestion.answer) gainScore();
+        loadNextQuestion();
+    })
+})
+
+gainScore = () => {
+    score += 10;
+}
+
+clearLastCheck = () => {
+    choices.forEach(choice => {
+        choice.parentElement.classList.remove('correct');
+        choice.parentElement.classList.remove('incorrect');
+    })
+}
+
+loadNextQuestion = () => {
+    document.getElementById('next-btn').onclick = () => {
+        clearLastCheck();
+        getNewQuestion();
+    }
+}
+
+
+startGame();
